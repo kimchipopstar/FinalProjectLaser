e42be4039502f3d6475bc38c4570c7f6da887be4
J.K <K_940201@hotmail.com>
2017-09-07 19:07:26 -0700

    before cleaning up code

--- FinalProjectLaser/GameScene.swift
+++ FinalProjectLaser/GameScene.swift
@@ -11,13 +11,18 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var testObject:SKSpriteNode?
-    var testLaser:SKSpriteNode?
-    var testButton:SKSpriteNode?
+
+
     let noCategory:UInt32 = 0
-    let objectCategory:UInt32 = 0b1
-    let laserCategory:UInt32 = 0b1 << 1
-    let buttonCategory:UInt32 = 0b1 << 2
+    let bulletLeftCategory:UInt32 = 0b1
+    let bulletUpCategory:UInt32 = 0b1 << 1
+    let leftButtonCategory:UInt32 = 0b1 << 2
+    let leftLaserCategory:UInt32 = 0b1 << 3
+
+    var bulletLeft:SKSpriteNode?
+    var bulletUp:SKSpriteNode?
+    var leftButton:SKSpriteNode?
+    var leftLaser:SKSpriteNode?
 
     override func didMove(to view: SKView) {
         
@@ -28,52 +33,76 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bgm.autoplayLooped = true
         self.addChild(bgm)
         
-//        let myShip:SKSpriteNode = self.childNode(withName: "ship") as! SKSpriteNode
-//        myShip.xScale = 0.5
-//        myShip.yScale = 0.5
-        
-//        let player:SKSpriteNode = SKSpriteNode(imageNamed: "Spaceship")
-//        self.addChild(player)
-//        player.position = CGPoint(x:100, y:100)
-        
-        testObject = self.childNode(withName: "testObject") as? SKSpriteNode
-        testObject?.physicsBody?.categoryBitMask = objectCategory
-        testObject?.physicsBody?.collisionBitMask = noCategory
-        testObject?.physicsBody?.contactTestBitMask = laserCategory
-        
-        testLaser = self.childNode(withName: "testLaser") as? SKSpriteNode
-        testLaser?.physicsBody?.categoryBitMask = laserCategory
-        testLaser?.physicsBody?.collisionBitMask = noCategory
-        testLaser?.physicsBody?.contactTestBitMask = objectCategory
-        
-        testButton = self.childNode(withName: "testButton") as? SKSpriteNode
-        testButton?.physicsBody?.categoryBitMask = buttonCategory
-        testButton?.physicsBody?.collisionBitMask = noCategory
-        testButton?.physicsBody?.contactTestBitMask = objectCategory
+        bulletLeft = self.childNode(withName: "bulletLeft") as? SKSpriteNode
+        bulletLeft?.physicsBody?.categoryBitMask = bulletLeftCategory
+        bulletLeft?.physicsBody?.collisionBitMask = noCategory
+        bulletLeft?.physicsBody?.contactTestBitMask = leftButtonCategory
+//        bulletLeft?.physicsBody = SKPhysicsBody(rectangleOf: (bulletLeft?.size)!)
+        bulletLeft?.physicsBody?.affectedByGravity = false
+        bulletLeft?.physicsBody?.isDynamic = true
+        bulletLeft?.physicsBody?.velocity = CGVector(dx: -100, dy: 0)
         
         
+        
+        bulletUp = self.childNode(withName: "bulletUp") as? SKSpriteNode
+        bulletUp?.physicsBody?.categoryBitMask = bulletUpCategory
+        bulletUp?.physicsBody?.collisionBitMask = noCategory
+        bulletUp?.physicsBody?.contactTestBitMask = leftLaserCategory
+//        bulletUp?.physicsBody = SKPhysicsBody(rectangleOf: (bulletUp?.size)!)
+        bulletUp?.physicsBody?.affectedByGravity = false
+        bulletUp?.physicsBody?.isDynamic = true
+        bulletUp?.physicsBody?.velocity = CGVector(dx: 0, dy: 200)
+        
+        leftButton = self.childNode(withName: "leftButton") as? SKSpriteNode
+        leftButton?.physicsBody?.categoryBitMask = leftButtonCategory
+        leftButton?.physicsBody?.collisionBitMask = noCategory
+        leftButton?.physicsBody?.contactTestBitMask = bulletLeftCategory
+//        leftButton?.physicsBody = SKPhysicsBody(rectangleOf: (leftButton?.size)!)
+        leftButton?.physicsBody?.isDynamic = true
+        
+        leftLaser = self.childNode(withName: "leftModel")?.childNode(withName: "leftLaser") as? SKSpriteNode
+        leftLaser?.physicsBody?.categoryBitMask = leftLaserCategory
+        leftLaser?.physicsBody?.collisionBitMask = noCategory
+        leftLaser?.physicsBody?.contactTestBitMask = bulletUpCategory
+//        leftLaser?.physicsBody? = SKPhysicsBody(rectangleOf: (leftLaser?.size)!)
+        leftLaser?.physicsBody?.isDynamic = true
+
+        
+
+
     }
+
+
     
     func didBegin(_ contact: SKPhysicsContact) {
+        print("contact")
+//        contact.bodyA.node.
+        let categoryA:UInt32 = contact.bodyA.categoryBitMask
+        let categoryB:UInt32 = contact.bodyB.categoryBitMask
+
+            if categoryA == bulletLeftCategory || categoryB == bulletLeftCategory{
+            let otherNode:SKSpriteNode = (categoryA == bulletLeftCategory) ? contact.bodyB.node as! SKSpriteNode : contact.bodyA.node as! SKSpriteNode
+                bulletLeftDidHitButton(with: otherNode)
+            } else if categoryA == leftLaserCategory || categoryB == leftLaserCategory{
+                let otherNode:SKSpriteNode = (categoryA == leftLaserCategory) ? contact.bodyB.node as! SKSpriteNode : contact.bodyA.node as! SKSpriteNode
+                bulletUpDidHitLaser(with: otherNode)
+            }
         
-        let cA:UInt32 = contact.bodyA.categoryBitMask
-        let cB:UInt32 = contact.bodyB.categoryBitMask
-        if cA == objectCategory || cB == objectCategory{
-            let otherNode:SKNode = (cA == objectCategory) ? contact.bodyB.node! : contact.bodyA.node!
-            objectDidCollide(with: otherNode as! SKSpriteNode)
-        }  else{
-            contact.bodyA.node?.removeFromParent()
+
         }
-        
-//        contact.bodyB.node?.removeFromParent()
+    
+
+
+    func bulletLeftDidHitButton(with other:SKSpriteNode){
+        other.color = UIColor.brown
+        leftLaser?.removeFromParent()
     }
     
-    func objectDidCollide(with other:SKSpriteNode){
-        let otherCategory = other.physicsBody?.categoryBitMask
-        if otherCategory == buttonCategory{
-            testButton?.color = UIColor.blue
-        }
+    func bulletUpDidHitLaser(with other:SKSpriteNode) {
+        other.removeFromParent()
     }
+    
+
 
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
