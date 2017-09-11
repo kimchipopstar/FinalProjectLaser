42953371610796319937f35af847c59659dbba0f
J.K <K_940201@hotmail.com>
2017-09-10 18:08:18 -0700

    test

--- FinalProjectLaser/GameScene.swift
+++ FinalProjectLaser/GameScene.swift
@@ -12,20 +12,21 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     //MAKR: - Categories
     let noCategory:UInt32 = 0
-    let bulletLeftCategory:UInt32 = 0b1
-    let bulletUpCategory:UInt32 = 0b1 << 1
-    let laserHubCategory:UInt32 = 0b1 << 2
-    let laserBeamCategory:UInt32 = 0b1 << 3
+
+    let laserHubCategory:UInt32 = 0b1
+    let laserBeamCategory:UInt32 = 0b1 << 1
+    let smallBallCategory:UInt32 = 0b1 << 2
+    let heroCategory:UInt32 = 0b1 << 3
 
     //MAKR: - node Properties
-    var bulletLeft:SKSpriteNode?
-    var bulletUp:SKSpriteNode?
-    
-    var laserHub:SKSpriteNode?
-    var laserBeam:SKSpriteNode?
+    var laserHub:SKSpriteNode = SKSpriteNode()
+    var laserBeam:SKSpriteNode = SKSpriteNode()
+    let laserHubTexture = SKTexture(imageNamed: "LaserHub")
+    let laserBeamTexture = SKTexture(imageNamed: "LaserBeam")
     
     var Hero = SKSpriteNode(imageNamed: "Laser")
     var background = SKSpriteNode()
+    var smallBall = SKSpriteNode()
 }
 
 extension GameScene{
@@ -55,6 +56,9 @@ extension GameScene{
         Hero.size = CGSize(width: 200, height: 200)
         Hero.position = CGPoint(x: 0, y: -600)
         Hero.zPosition = 4
+        Hero.physicsBody?.categoryBitMask = heroCategory
+        Hero.physicsBody?.collisionBitMask = noCategory
+        Hero.physicsBody?.contactTestBitMask = laserBeamCategory
         
         self.addChild(Hero)
         
@@ -80,15 +84,18 @@ extension GameScene{
         for touch in (touches ) {
             let location = touch.location(in: self)
             
-            let SmallBall = SKSpriteNode(imageNamed: "Projectile1")
-            SmallBall.position = Hero.position
-            SmallBall.size = CGSize(width: 30, height: 30)
-            SmallBall.physicsBody = SKPhysicsBody(circleOfRadius: SmallBall.size.width / 2)
-            SmallBall.physicsBody?.affectedByGravity = true
-            SmallBall.zPosition = 3
-            SmallBall.name = "SmallBall"
+            smallBall = SKSpriteNode(imageNamed: "Projectile1")
+            smallBall.position = Hero.position
+            smallBall.size = CGSize(width: 30, height: 30)
+            smallBall.physicsBody = SKPhysicsBody(circleOfRadius: smallBall.size.width / 2)
+            smallBall.physicsBody?.affectedByGravity = true
+            smallBall.zPosition = 3
+            smallBall.name = "SmallBall"
+            smallBall.physicsBody?.categoryBitMask = smallBallCategory
+            smallBall.physicsBody?.collisionBitMask  = noCategory
+            smallBall.physicsBody?.contactTestBitMask = laserBeamCategory | laserHubCategory
             
-            self.addChild(SmallBall)
+            self.addChild(smallBall)
             
             var dx = CGFloat(location.x - Hero.position.x)
             var dy = CGFloat(location.y - Hero.position.y)
@@ -100,7 +107,7 @@ extension GameScene{
             
             let vector = CGVector(dx: 60.0 * dx, dy: 60.0 * dy)
             
-            SmallBall.physicsBody?.applyImpulse(vector)
+            smallBall.physicsBody?.applyImpulse(vector)
             
         }
     }
@@ -112,31 +119,35 @@ extension GameScene{
     func laserHubSetUp()
     {
         laserHub = SKSpriteNode(imageNamed: "LaserHub")
-        laserHub?.zPosition = 1
-        laserHub?.setScale(0.5)
-        laserHub?.position = CGPoint(x: -320, y: 0)
-        self.addChild(laserHub!)
-//        laserHub = self.childNode(withName: "laserHub") as? SKSpriteNode
-//        laserHub?.physicsBody?.categoryBitMask = laserHubCategory
-//        laserHub?.physicsBody?.collisionBitMask = noCategory
-//        laserHub?.physicsBody?.contactTestBitMask = bulletLeftCategory
-//        laserHub?.physicsBody?.affectedByGravity = false
-//        laserHub?.physicsBody?.isDynamic = true
+        laserHub.zPosition = 1
+        laserHub.setScale(0.5)
+        laserHub.position = CGPoint(x: -320, y: 0)
+        laserHub.physicsBody?.categoryBitMask = laserHubCategory
+        laserHub.physicsBody?.collisionBitMask = noCategory
+        laserHub.physicsBody?.contactTestBitMask = smallBallCategory
+        laserHub.physicsBody = SKPhysicsBody(texture: laserHubTexture, size:laserHub.size)
+        laserHub.physicsBody?.affectedByGravity = false
+        laserHub.physicsBody?.isDynamic = false
+        self.addChild(laserHub)
+
+
     }
     
     func laserBeamSetUp()
     {
         laserBeam = SKSpriteNode(imageNamed: "LaserBeam")
-        laserBeam?.zPosition = 2
-        laserBeam?.size.width = 1250
-        laserBeam?.position = CGPoint(x: 650, y: 80)
-        laserHub?.addChild(laserBeam!)
-//        laserBeam = self.childNode(withName: "laserHub")?.childNode(withName: "laserBeam") as? SKSpriteNode
-//        laserBeam?.physicsBody?.categoryBitMask = laserBeamCategory
-//        laserBeam?.physicsBody?.collisionBitMask = noCategory
-//        laserBeam?.physicsBody?.contactTestBitMask = bulletUpCategory
-//        laserBeam?.physicsBody?.affectedByGravity = false
-//        laserBeam?.physicsBody?.isDynamic = true
+        laserBeam.zPosition = 2
+        laserBeam.size.width = 1250
+        laserBeam.position = CGPoint(x: 650, y: 80)
+        laserBeam.physicsBody?.categoryBitMask = laserBeamCategory
+        laserBeam.physicsBody?.collisionBitMask = noCategory
+        laserBeam.physicsBody?.contactTestBitMask = smallBallCategory | heroCategory
+        laserBeam.physicsBody = SKPhysicsBody(texture: laserBeamTexture, size: laserBeam.size)
+        laserBeam.physicsBody?.affectedByGravity = false
+        laserBeam.physicsBody?.isDynamic = false
+        laserHub.addChild(laserBeam)
+
+
     }
 
     
@@ -148,13 +159,14 @@ extension GameScene{
     
     func didBegin(_ contact: SKPhysicsContact)
     {
-        
+        print("contact")
+
         let categoryA:UInt32 = contact.bodyA.categoryBitMask
         let categoryB:UInt32 = contact.bodyB.categoryBitMask
         
-        if categoryA == bulletLeftCategory || categoryB == bulletLeftCategory
+        if categoryA == smallBallCategory || categoryB == smallBallCategory
         {
-            let otherNode:SKSpriteNode = (categoryA == bulletLeftCategory) ? contact.bodyB.node as! SKSpriteNode : contact.bodyA.node as! SKSpriteNode
+            let otherNode:SKSpriteNode = (categoryA == smallBallCategory) ? contact.bodyB.node as! SKSpriteNode : contact.bodyA.node as! SKSpriteNode
             bulletLeftDidHitButton(with: otherNode)
             print("contactHub")
         }
@@ -171,7 +183,7 @@ extension GameScene{
     {
 //        other.color = UIColor.brown
         other.physicsBody?.isDynamic = false
-        laserBeam?.removeFromParent()
+        laserBeam.removeFromParent()
     }
     
     func bulletUpDidHitLaser(with other:SKSpriteNode)
@@ -185,7 +197,7 @@ extension GameScene{
 extension GameScene {
     
     func laserInitialPosition(){
-        laserHub?.position = CGPoint(x: (-self.frame.width / 2) + ((laserHub?.frame.width)! / 2), y: (self.frame.height / 2) + ((laserHub?.frame.height)!/2))
+        laserHub.position = CGPoint(x: (-self.frame.width / 2) + ((laserHub.frame.width) / 2), y: (self.frame.height / 2) + ((laserHub.frame.height)/2))
     }
     
     func setBackgroundMusic(atScene:SKScene, fileName:String)
