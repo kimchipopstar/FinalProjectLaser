94addb24446eda4b4b56a9f5350dce241170f9c3
Tye Blackie <blackie.tye@gmail.com>
2017-09-19 18:16:32 -0700

    Spawning and acceleration fixed, hero damage

--- FinalProjectLaser/MainMenu.swift
+++ FinalProjectLaser/MainMenu.swift
@@ -1,18 +1,13 @@
 //
-//  MainMenu.swift
+//  MainMenue.swift
 //  FinalProjectLaser
 //
-//  Created by Jaewon Kim on 2017-09-05.
+//  Created by Tye Blackie on 2017-09-19.
 //  Copyright © 2017 Jaewon Kim. All rights reserved.
 //
 
 import UIKit
-import SpriteKit
 
-class MainMenu: SKScene {
-    
-    override func didMove(to view: SKView) {
-        print("did launch")
-    }
+class MainMenue: SKScene {
 
 }
