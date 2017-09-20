b02e063b745fcf1425e778c02f6e9edd3538dd06
Varninder Rai <varninder@openmailbox.org>
2017-09-19 18:59:29 -0700

    Add Mainmenu and leaderboard

---
+++ FinalProjectLaser/GameOverViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameOverViewController.swift
+//  FinalProjectLaser
+//
+//  Created by Livleen Rai on 2017-09-13.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+import UIKit
+
+class GameOverViewController: SKScene {
+
+}
---
+++ FinalProjectLaser/LeaderBoardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaderBoardTableViewCell.swift
+//  FinalProjectLaser
+//
+//  Created by Livleen Rai on 2017-09-13.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+import UIKit
+
+class LeaderBoardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
